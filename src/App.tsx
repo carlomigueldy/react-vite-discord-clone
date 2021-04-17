087425ef0152974b7f723b490c5855d7840d9d51
Carlo Miguel Dy <carlomigueldy@gmail.com>
2021-04-17 10:11:44 +0800

    feat: installed `chakra-ui` and custom font "Sora"

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import React, { useState } from "react";
 import "./App.css";
 import AppRouteProvider from "./routes/AppRouteProvider";
 
+import "@fontsource/sora"
+
+const colors = {
+  brand: {
+    900: "#1a365d",
+    800: "#153e75",
+    700: "#2a69ac",
+  },
+};
+
+const theme = extendTheme({
+  colors,
+  fonts: {
+    heading: "Sora",
+    body: "Sora",
+  },
+});
+
 function App() {
   return (
-    <>
+    <ChakraProvider theme={theme}>
       <AppRouteProvider></AppRouteProvider>
-    </>
+    </ChakraProvider>
   );
 }
 
