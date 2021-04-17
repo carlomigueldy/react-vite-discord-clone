4a542c82c1775cb5ab368e19548fbbe331d55314
Carlo Miguel Dy <carlomigueldy@gmail.com>
2021-04-17 19:01:56 +0800

    feat: improved layout

--- src/ui/layouts/MainLayout.tsx
+++ src/ui/layouts/MainLayout.tsx
@@ -1,4 +1,13 @@
-import { Avatar, Box, Divider, Heading } from "@chakra-ui/react";
+import { AddIcon, AtSignIcon, CalendarIcon, LinkIcon } from "@chakra-ui/icons";
+import {
+  Avatar,
+  Box,
+  Divider,
+  Heading,
+  IconButton,
+  Input,
+  Textarea,
+} from "@chakra-ui/react";
 import React from "react";
 
 type Props = {
@@ -10,33 +19,49 @@ export default function MainLayout({ children }: Props) {
     <Box height="100vh" display="flex" justifyContent="space-between">
       <ServerSidebar />
       <CategorySidebar />
-      <Box
-        backgroundColor="blue"
-        width="100%"
-        display="flex"
-        flexDirection="column"
-        justifyContent="space-between"
-      >
-        <Box height="50px" backgroundColor="red" />
-
-        <Box flexGrow={1} display="flex" justifyContent="space-between">
-          <MainContent />
-
-          <MemberSidebar />
-        </Box>
-      </Box>
+      <MainContent />
     </Box>
   );
 }
 
 function MemberSidebar() {
-  return <Box width="325px" backgroundColor="yellow"></Box>;
+  return <Box width="325px" backgroundColor="gray.700"></Box>;
 }
 
 function MainContent() {
   return (
     <Box
-      // backgroundColor="green.500"
+      backgroundColor="gray.900"
+      width="100%"
+      display="flex"
+      flexDirection="column"
+      justifyContent="space-between"
+    >
+      <MainTopbar />
+
+      <Box flexGrow={1} display="flex" justifyContent="space-between">
+        <ChatContainer />
+
+        <MemberSidebar />
+      </Box>
+    </Box>
+  );
+}
+
+function MainTopbar() {
+  return (
+    <Box
+      height="50px"
+      backgroundColor="gray.800"
+      borderBottomColor="gray"
+      borderBottomWidth="1px"
+    />
+  );
+}
+
+function ChatContainer() {
+  return (
+    <Box
       width="100%"
       display="flex"
       justifyContent="space-between"
@@ -48,6 +73,7 @@ function MainContent() {
         flexDirection="column"
         height="0px"
         overflowY="scroll"
+        paddingX="15px"
       >
         <PlaceholderItem />
         <PlaceholderItem />
@@ -55,7 +81,57 @@ function MainContent() {
         <PlaceholderItem />
         <PlaceholderItem />
       </Box>
-      <Box height="60px" backgroundColor="orange"></Box>
+
+      <ChatInput />
+    </Box>
+  );
+}
+
+function ChatInput() {
+  return (
+    <Box height="70px" paddingX="15px" backgroundColor="gray.900">
+      <Box
+        display="flex"
+        justifyContent="space-around"
+        backgroundColor="gray.600"
+        padding="5px"
+        borderRadius="8px"
+      >
+        <IconButton
+          aria-label="Attach files"
+          borderRadius="50%"
+          size="sm"
+          marginX="5px"
+          icon={<AddIcon />}
+        />
+        <Input
+          height="35px"
+          backgroundColor="gray.600"
+          color="white"
+          borderColor="none"
+        />
+        <IconButton
+          aria-label="Attach files"
+          borderRadius="50%"
+          size="sm"
+          marginX="5px"
+          icon={<CalendarIcon />}
+        />
+        <IconButton
+          aria-label="Select giphy"
+          borderRadius="50%"
+          size="sm"
+          marginX="5px"
+          icon={<LinkIcon />}
+        />
+        <IconButton
+          aria-label="Select emoji"
+          borderRadius="50%"
+          size="sm"
+          marginX="5px"
+          icon={<AtSignIcon />}
+        />
+      </Box>
     </Box>
   );
 }
@@ -75,16 +151,16 @@ function ServerSidebar() {
       alignItems="center"
       overflowY="hidden"
     >
-      <Avatar />
-      <Divider marginY="15px" />
-      <Avatar backgroundColor="red" marginY="5px" />
-      <Avatar backgroundColor="blue" marginY="5px" />
-      <Avatar backgroundColor="green" marginY="5px" />
-      <Avatar backgroundColor="orange" marginY="5px" />
-      <Avatar backgroundColor="pink" marginY="5px" />
-      <Avatar backgroundColor="violet" marginY="5px" />
-      <Avatar backgroundColor="cyan" marginY="5px" />
-      <Avatar backgroundColor="yellow" marginY="5px" />
+      <Avatar height="40px" width="40px" />
+      <Divider marginY="15px" width="30px" />
+      <Avatar size="md" cursor="pointer" backgroundColor="red" marginY="5px" />
+      <Avatar size="md" cursor="pointer" backgroundColor="blue" marginY="5px" />
+      <Avatar size="md" cursor="pointer" backgroundColor="green" marginY="5px" />
+      <Avatar size="md" cursor="pointer" backgroundColor="orange" marginY="5px" />
+      <Avatar size="md" cursor="pointer" backgroundColor="pink" marginY="5px" />
+      <Avatar size="md" cursor="pointer" backgroundColor="violet" marginY="5px" />
+      <Avatar size="md" cursor="pointer" backgroundColor="cyan" marginY="5px" />
+      <Avatar size="md" cursor="pointer" backgroundColor="yellow" marginY="5px" />
     </Box>
   );
 }
@@ -92,7 +168,12 @@ function ServerSidebar() {
 function CategorySidebar() {
   return (
     <Box width="400px" backgroundColor="gray.700">
-      <Box height="50px" backgroundColor="red" />
+      <Box
+        height="50px"
+        backgroundColor="gray.800"
+        borderBottomColor="gray"
+        borderBottomWidth="1px"
+      />
       <Heading>Hey</Heading>
     </Box>
   );
