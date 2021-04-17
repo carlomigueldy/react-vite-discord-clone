e662df570c4aaf5be6cbdc9e3b5ee9593ba8dc0b
Carlo Miguel Dy <carlomigueldy@gmail.com>
2021-04-17 10:05:20 +0800

    feat: added `NotFoundView`

--- src/routes/AppRouteProvider.tsx
+++ src/routes/AppRouteProvider.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import IndexView from "../ui/views/IndexView";
+import NotFoundView from "../ui/views/NotFoundView";
 
 const AppRouteProvider = () => {
   return (
     <Router>
       <Switch>
         <Route exact path="/" component={IndexView}></Route>
+        <Route path="*" component={NotFoundView}></Route>
       </Switch>
     </Router>
   );
---
+++ src/ui/views/NotFoundView.tsx
@@ -0,0 +1,5 @@
+import React from "react"
+
+export default function NotFoundView() {
+  return <h1>Not Found</h1>
+}
