38276acffabd9898ef3506f997c0c33f894fe508
Carlo Miguel Dy <carlomigueldy@gmail.com>
2021-04-17 10:05:52 +0800

    feat: read all from servers table

--- src/app/supabase.ts
+++ src/app/supabase.ts
@@ -1,6 +1,15 @@
 import { createClient } from "@supabase/supabase-js";
 
+const SUPABASE_URL = "https://bfufbgvjvdlyoabzoycp.supabase.co";
+const SUPABASE_SECRET_KEY =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYxODUzNzU3NSwiZXhwIjoxOTM0MTEzNTc1fQ.osjHpVmTaL8xfQRq9rDD9FB7g7a5zKXOfvGum4j_-rY";
+
 export const supabase = createClient(
-  process.env.SUPABASE_URL || "",
-  process.env.SUPBASE_SECRET_KEY || ""
+  SUPABASE_URL || "",
+  SUPABASE_SECRET_KEY || ""
 );
+
+// export const supabase = createClient(
+//   process.env.SUPABASE_URL || "",
+//   process.env.SUPBASE_SECRET_KEY || ""
+// );
--- src/ui/views/IndexView.tsx
+++ src/ui/views/IndexView.tsx
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { supabase } from "../../app/supabase";
 
 function IndexView() {
+  const [servers, setServers] = useState<any[] | null>([]);
+
+  useEffect(() => {
+    supabase.from("servers").then((res) => {
+      setServers(res.data);
+    });
+  }, []);
+
   return (
     <>
       <h1>Index View</h1>
+
+      <pre>{JSON.stringify(servers, null, 2)}</pre>
     </>
   );
 }
