fe2261e2d99e8bdd288ddafca6373e4047054752
Carlo Miguel Dy <carlomigueldy@gmail.com>
2021-04-18 17:05:05 +0800

    refactor: color

--- src/ui/components/AppPlaceholderItem.tsx
+++ src/ui/components/AppPlaceholderItem.tsx
@@ -1,18 +1,19 @@
 import { Avatar, Box } from "@chakra-ui/react";
 import React from "react";
+import { colors } from "../theme/colors";
 
 export default function AppPlaceholderItem() {
   return (
     <Box display="flex" marginY="15px">
-      <Avatar></Avatar>
+      <Avatar backgroundColor={colors.grayMedium}></Avatar>
 
       <Box width="15px"></Box>
 
       <Box
         height="225px"
         width="500px"
-        borderRadius="sm"
-        backgroundColor="gray.500"
+        borderRadius="5px"
+        backgroundColor={colors.grayMedium}
       ></Box>
     </Box>
   );
